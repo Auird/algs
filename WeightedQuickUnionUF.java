86b9d5f143db4ddcf269e56e501cf795daf08db9
Xin Cheng <aui.cheng.010@gmail.com>
2018-01-19 22:16:04 -0800

    WeightedQuickUnion Implementation.

---
+++ WeightedQuickUnionUF.java
@@ -0,0 +1,48 @@
+package algs;
+
+public class WeightedQuickUnionUF {
+    private int[] id;
+    private int[] sz;
+    private int components;
+
+    public WeightedQuickUnionUF(int N) {
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+        components = N;
+    }
+
+    public int find(int p){
+        while(p != id[p]){
+            p = id[p];
+        }
+        return p;
+    }
+
+    public boolean isConnected(int p, int q){
+        return find(p) == find(q);
+    }
+
+    public int count(){
+        return components;
+    }
+
+    public void weightedQuickUnion(int p, int q){
+        if(isConnected(p,q)){
+            return;
+        }
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if(sz[pRoot] < sz[qRoot]) {
+            id[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
+        }else{
+            id[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
+        }
+    }
+
+}
